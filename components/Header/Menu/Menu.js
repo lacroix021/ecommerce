541dc67e5dcf9bb8a7b0617865845d48b5b5e603
lacroix021 <oscaryagami86@gmail.com>
2021-09-22 17:09:41 -0500

    se agrega paneles de usuario login y register, register ya conecta con la base de datos de strapi

--- components/Header/Menu/Menu.js
+++ components/Header/Menu/Menu.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import {Container, Menu, Grid, Icon, Label} from "semantic-ui-react";
 import Link from "next/link";
+import BasicModal from "../../Modal/BasicModal/BasicModal";
+import Auth from "../../Auth";
 
 export default function MenuWeb() {
+    const[showModal, setShowModal] = useState(false);
+    const [titleModal, setTitleModal] = useState("Inicia Sesion")
+
+    const onShowModal = () => setShowModal(true);
+
+    const onCloseModal = () => setShowModal(false);
+
     return (
         <div className="menu">
             <Container>
@@ -10,10 +20,18 @@ export default function MenuWeb() {
                         <MenuPlatforms/>
                     </Grid.Column>
                     <Grid.Column className="menu__right" width ={10}>
-                        <MenuOptions />
+                        <MenuOptions onShowModal={onShowModal}/>
                     </Grid.Column>
                 </Grid>
             </Container>
+            <BasicModal 
+                show={showModal} 
+                setShow={setShowModal} 
+                title={titleModal}
+                size="small"
+            >
+                <Auth onCloseModal={onCloseModal} setTitleModal={setTitleModal}/>
+            </BasicModal>
         </div>
     )
 }
@@ -36,13 +54,15 @@ function MenuPlatforms(){
     )
 }
 
-function MenuOptions(){
+function MenuOptions(props){
+    const {onShowModal} = props;
+
     return(
         <Menu>
-            <Menu.Item>
-            <Icon name = "user outline" />
-            Mi cuenta
-        </Menu.Item>
+            <Menu.Item onClick={onShowModal}>
+                <Icon name = "user outline" />
+                Mi cuenta
+            </Menu.Item>
         </Menu>
     )
 }
