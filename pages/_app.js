ac22446f22701540c8fc85f99823d7a720c620ec
lacroix021 <oscaryagami86@gmail.com>
2021-09-24 11:28:01 -0500

    se implementa menu login, de momento hay un error de comunicacion hhtp post ya que no permite validar correo con la base de datos.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,51 @@
+import React, {useState, useEffect, useMemo} from "react";
 import {ToastContainer} from "react-toastify";
-
+import jwtDecode from "jwt-decode";
+import AuthContext from "../context/AuthContext";
+import {setToken, getToken} from "../api/token";
 import "../scss/global.scss";
 import 'semantic-ui-css/semantic.min.css';
 import "react-toastify/dist/ReactToastify.css";
 
 export default function MyApp({ Component, pageProps }) {
+  const [auth, setAuth] = useState(undefined)
+  const [reloadUser, setReloadUser] = useState(false)
+
+  useEffect(() => {
+    const token = getToken();
+    if(token){
+      setAuth({
+        token,
+        idUser: jwtDecode(token).id
+      })
+    }else{
+      setAuth(null);
+    }
+  }, [reloadUser]);
+
+  const login = (token) =>{
+    setToken(token);
+      setAuth({
+        token,
+        idUser: jwtDecode(token).id
+      })
+  };
+
+  const authData = useMemo(
+    () => ({
+      auth,
+      login,
+      logout: () => null,
+      setReloadUser,
+    }), 
+    [auth]
+  );
+
+  if(auth === undefined) return null;
+
+
   return(
-    <>
+    <AuthContext.Provider value={authData}>
       <Component {...pageProps} /> 
       <ToastContainer
         position="top-right"
@@ -19,6 +58,6 @@ export default function MyApp({ Component, pageProps }) {
         draggable
         pauseOnHover
       />
-    </>
+    </AuthContext.Provider>
   );
 }
